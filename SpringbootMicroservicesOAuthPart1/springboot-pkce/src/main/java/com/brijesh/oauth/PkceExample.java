f7d8abda228c343c786dec96d5ef3bbdc3f13ed8
brij2804 <brij2804@users.noreply.github.com>
2022-04-24 10:39:42 +0530

    PKCE code verifier and code challenge generator

--- SpringbootMicroservicesOAuthPart1/springboot-pkce/src/main/java/com/brijesh/oauth/PkceExample.java
+++ SpringbootMicroservicesOAuthPart1/springboot-pkce/src/main/java/com/brijesh/oauth/PkceExample.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.brijesh.oauth;
 
 import java.io.UnsupportedEncodingException;
@@ -10,10 +6,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author sergeykargopolov
- */
+
 public class PkceExample {
 
     public static void main(String[] args) {
