15b4ee2718aaa9002c6de7bfe49739431a120419
brij2804 <brij2804@users.noreply.github.com>
2022-04-26 16:33:45 +0530

    Configured HTTP Security for Scope-based access control

--- SpringbootMicroservicesOAuthPart3/springboot-oauth-resourceserver/src/main/java/com/brijesh/oauth/security/WebSecurity.java
+++ SpringbootMicroservicesOAuthPart3/springboot-oauth-resourceserver/src/main/java/com/brijesh/oauth/security/WebSecurity.java
@@ -1,4 +1,24 @@
 package com.brijesh.oauth.security;
 
-public class WebSecurity {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private final Log logger = LogFactory.getLog(WebSecurity.class);
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        this.logger.debug("Using default configure(HttpSecurity). If subclassed this will potentially override subclass configure(HttpSecurity).");
+        http.authorizeRequests((requests) -> {
+            ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)requests.antMatchers(HttpMethod.GET,"/users/status/check").hasAuthority("SCOPE_profile").anyRequest()).authenticated();
+        });
+        http.oauth2ResourceServer().jwt();
+    }
 }
