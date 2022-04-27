f0048f15d18022ce9364e8940bb7e92e00ed9f92
brij2804 <brij2804@users.noreply.github.com>
2022-04-27 15:20:39 +0530

    Method level security annotations

--- SpringbootMicroservicesOAuthPart5/springboot-oauth-resourceserver/src/main/java/com/brijesh/oauth/controller/UserController.java
+++ SpringbootMicroservicesOAuthPart5/springboot-oauth-resourceserver/src/main/java/com/brijesh/oauth/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.brijesh.oauth.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -11,5 +10,11 @@ public class UserController {
     @GetMapping("/status/check")
     public String status(){
         return "working...";
+    }
+
+    @Secured("ROLE_developer")
+    @DeleteMapping(path="/{id}")
+    public String deleteUser(@PathVariable String id){
+        return "Deleted user with id "+id;
     }
 }
--- SpringbootMicroservicesOAuthPart5/springboot-oauth-resourceserver/src/main/java/com/brijesh/oauth/security/WebSecurity.java
+++ SpringbootMicroservicesOAuthPart5/springboot-oauth-resourceserver/src/main/java/com/brijesh/oauth/security/WebSecurity.java
@@ -3,12 +3,14 @@ package com.brijesh.oauth.security;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
@@ -25,6 +27,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
             ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)requests.antMatchers(HttpMethod.GET,"/users/status/check")
                      .hasRole("developer")
                     //.hasAnyRole("developer","user")
+                    //.hasAnyAuthority("ROLE_developer")
                     .anyRequest()).authenticated();
         });
         http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter);
