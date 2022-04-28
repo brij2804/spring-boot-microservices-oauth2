4bc585330540aafe6514fc34a5bbdc13614b706b
brij2804 <brij2804@users.noreply.github.com>
2022-04-28 17:59:01 +0530

    Method level security annotations

--- SpringbootMicroservicesOAuthPart5/springboot-oauth-resourceserver/src/main/java/com/brijesh/oauth/controller/UserController.java
+++ SpringbootMicroservicesOAuthPart5/springboot-oauth-resourceserver/src/main/java/com/brijesh/oauth/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.brijesh.oauth.controller;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,5 +20,17 @@ public class UserController {
     @DeleteMapping(path="/{id}")
     public String deleteUser(@PathVariable String id){
         return "Deleted user with id "+id;
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_developer')")
+    @DeleteMapping(path="/preauthorize/{id}")
+    public String deleteUserPreAuthorize(@PathVariable String id){
+        return "Deleted user with id "+id;
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
+    @DeleteMapping(path="/preauthorize1/{id}")
+    public String deleteUserPreAuthorizeExpression(@PathVariable String id, @AuthenticationPrincipal Jwt jwt){
+        return "Deleted user with id "+id + " and JWT subject " + jwt.getSubject();
     }
 }
